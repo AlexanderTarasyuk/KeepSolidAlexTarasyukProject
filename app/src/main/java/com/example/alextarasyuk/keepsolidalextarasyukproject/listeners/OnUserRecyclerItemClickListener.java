a3b1f28b436fd9eed77dce95cc27dfa754d5353f
AlexandrTarasyuk <alextarasyuk79@gmail.com>
2017-08-02 22:00:15 +0300

    Recycler View, adapters, model are added

---
+++ app/src/main/java/com/example/alextarasyuk/keepsolidalextarasyukproject/listeners/OnUserRecyclerItemClickListener.java
@@ -0,0 +1,12 @@
+package com.example.alextarasyuk.keepsolidalextarasyukproject.listeners;
+
+import android.view.View;
+
+/**
+ * Created by t420 on 25.07.2017.
+ */
+
+public interface OnUserRecyclerItemClickListener {
+
+    public void onItemClick(View v, int position);
+}
